d83bf519617d4b37de568f587b0bc1555f33bf91
inconsolavel <127897196+inconsolavel@users.noreply.github.com>
2025-05-02 18:55:34 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "jackbarcafe-b8fa8.firebaseapp.com",
+  projectId: "jackbarcafe-b8fa8",
+  storageBucket: "jackbarcafe-b8fa8.appspot.com",
+  messagingSenderId: "849215333925",
+  appId: "1:849215333925:web:e02cb0d11c590af32201cc"
+};
