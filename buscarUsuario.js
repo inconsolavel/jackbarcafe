1875002615d0f32af83c51325aadebabb19cc104
inconsolavel <127897196+inconsolavel@users.noreply.github.com>
2026-08-01 18:50:41 -0300

    Add function to search user by phone number

---
+++ buscarUsuario.js
@@ -0,0 +1,37 @@
+async function buscarUsuario() {
+
+    const telefone = document
+        .getElementById("telefone")
+        .value
+        .replace(/\D/g, "");
+
+    const snap = await db
+        .collection("usuarios")
+        .where("telefone", "==", telefone)
+        .limit(1)
+        .get();
+
+    if (snap.empty) {
+
+        alert("Usuário não encontrado.");
+        return;
+
+    }
+
+    snap.forEach(doc => {
+
+        usuarioSelecionado = doc.id;
+        dadosUsuario = doc.data();
+
+    });
+
+    document.getElementById("nome").innerHTML =
+        dadosUsuario.nome;
+
+    document.getElementById("tel").innerHTML =
+        dadosUsuario.telefone;
+
+    document.getElementById("points").innerHTML =
+        dadosUsuario.points;
+
+}
